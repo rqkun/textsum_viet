fc84220daa18ab1f1077d51082ec88c0c82ae354
raqkun <nhkduy0809@gmail.com>
2024-06-23 21:53:29 +0700

    compact laws_sum UI

--- pages/📓 Laws.py
+++ pages/📓 Laws.py
@@ -38,54 +38,60 @@ with st.sidebar:
     length_penalty = st.slider("Length penalty", 0.5, 1.0, 1.0, 0.1)
     num_beams = st.slider("Number of beams", 1, 10, 8)
     input_option = st.selectbox("Text or URL?", ("Text", "URL"))
+
 #MAIN
-txt = st.text_area("Input text",label_visibility="collapsed")
+
+txt = st.text_area("".join(["Input ",input_option," for summary. Press Ctrl + Enter to Submit"]))
+
 progress_text = "Enter the text for summary."
 
-col1,col2=st.columns(2)
-with col1:
-    my_bar = st.progress(0, text=progress_text)
-with col2:
-    formbtn = st.button("Submit",use_container_width=True,type="primary")
+# col1,col2=st.columns(2)
+# placeholder_for_prog = st.empty()
+# with placeholder_for_prog:
+#     with col1:
+#         my_bar = st.progress(0, text=progress_text)
+#     with col2:
+#         formbtn = st.button("Submit",use_container_width=True,type="primary")
 
-if "formbtn_state" not in st.session_state:
-    st.session_state.formbtn_state = False
 
-if formbtn or st.session_state.formbtn_state:
-    st.session_state.formbtn_state = True
+# if "formbtn_state" not in st.session_state:
+#     st.session_state.formbtn_state = False
 
-    if txt != "":
-        if input_option == "URL":
-            if utils.is_url(txt) == False:
-                st.error("Input is not an url!")
-            law_from_url = utils.get_law_from_url(txt)
-            txt = law_from_url
-        else:
-            if utils.is_url(txt) == True:
-                st.error("Input is not a text!")
-        dieu_dict = utils.create_Dict(txt)
-        if (len(dieu_dict) >0):
-            btns=[]
-            captions = []
-            for key, val in dieu_dict.items():
-                btns.append(key)
-                captions.append(val)
-            source = dieu_dict.get(btns[0])
-            with st.form(key = 'choice'):
+# if formbtn or st.session_state.formbtn_state:
+#     st.session_state.formbtn_state = True
+
+if txt != "":
+    if input_option == "URL":
+        if utils.is_url(txt) == False:
+            st.error("Input is not an url!")
+        law_from_url = utils.get_law_from_url(txt)
+        txt = law_from_url
+    else:
+        if utils.is_url(txt) == True:
+            st.error("Input is not a text!")
+    dieu_dict = utils.create_Dict(txt)
+    if (len(dieu_dict) >0):
+        btns=[]
+        captions = []
+        for key, val in dieu_dict.items():
+            btns.append(key)
+            captions.append(val)
+        source = dieu_dict.get(btns[0])
+        with st.form(key = 'choice'):
+            with st.expander("Expand for options..."):
                 placeholder_for_radio = st.empty()
-                placeholder_for_texta = st.empty()
-                submit_form = st.form_submit_button(label="Summary", help="Click to Summary", use_container_width=True)
-                
-            with placeholder_for_radio:
-                genre = st.radio("Which part to summarize: ", btns, index=0)
-                source = dieu_dict.get(genre)
+            placeholder_for_texta = st.empty()
+            my_bar = st.progress(0, text=progress_text)
+            submit_form = st.form_submit_button(label="Summary", help="Click to Summary", use_container_width=True)
             
-            with placeholder_for_texta:
-                st.text_area("input",label_visibility="hidden",value=source,disabled=True)
-        else:
-            with st.form(key = 'choice'):
-                submit_form = st.form_submit_button(label="Summary", help="Click to Summary", use_container_width=True)
-
+        with placeholder_for_radio:
+            genre = st.radio("Which part to summarize: ", btns, index=0)
+            source = dieu_dict.get(genre)
+        
+        with placeholder_for_texta:
+            st.text_area("input",label_visibility="hidden",value=source,disabled=True)
+        #with st.form(key = 'choice'):
+        #submit_form = st.form_submit_button(label="Summary", help="Click to Summary", use_container_width=True)
         if submit_form:
             my_bar.progress(0, text="Applying TF-IDF...")
             dieu = ""
@@ -101,7 +107,10 @@ if formbtn or st.session_state.formbtn_state:
 
             my_bar.progress(100, text="Finish!")
             showdialog(summary[0]["summary_text"])
-            
-                
     else:
-        st.error("Please enter text!")
+        if input_option == "URL":
+            st.error("URL doesn't contain any laws data!")
+        else:
+            st.error("Invalid Input!")
+else:
+    st.error("Please enter text!")
